709c404539ccbd378aa51d46cca3cfc9cb4d5266
MollyRich1 <131802119+MollyRich1@users.noreply.github.com>
2024-03-19 16:45:12 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+// //slideshow stuff attempt with added start and stop button
+// const pausePlayBtns = document.querySelectorAll('.pause-play-btn');
+// const slideTracks = document.querySelectorAll('.slide-track');
+
+// pausePlayBtns.forEach((btn, index) => {
+//   let isPlaying = true;
+//   // how does the index reset, it doesn't??
+//   //make an if statement and reset it back to 0
+//   const slideTrack = slideTracks[index];
+
+//   btn.addEventListener('click', () => {
+//     if (isPlaying) {
+//       slideTrack.style.animationPlayState = 'paused';
+//       btn.textContent = 'Play';
+//     } else {
+//       slideTrack.style.animationPlayState = 'running';
+//       btn.textContent = 'Pause';
+//     }
+//     isPlaying = !isPlaying;
+//   });
+// });
+
+
+
+
+// //modal image stuff 1
+// var modal = document.getElementById("myModal");
+
+// var img = document.getElementById("Monteverde_main");
+// var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("caption");
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// }
+
+
+// var span = document.getElementsByClassName("close")[0];
+
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+
